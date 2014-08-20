f45134a5c260b721ebcd6298aa9deec18ec2f7ea
Chris Sheppard <cloud9@chrissheppard.co.uk>
2014-08-20 21:23:43 +0000

    Adding some database test...

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/dokku-test');
+
+
+
+// Create a schema for our data
+var MessageSchema = new mongoose.Schema({
+  message: String,
+  date: Date
+});
+
+// Use the schema to register a model with MongoDb
+mongoose.model('Message', MessageSchema); 
+var Message = mongoose.model('Message'); 
+
+
+
+
+
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+    //res.send('respond with a resource');
+  
+    Message.find().sort('date', -1).execFind(function (arr, data) {
+        res.send(data);
+    });
+  
+});
+
+
+router.get('/add', function(req, res) {
+    var message = new Message();
+    message.message = req.params.message;
+    message.date = new Date();
+    message.save(function () {
+        res.send(req.body);
+    });
 });
 
 module.exports = router;
